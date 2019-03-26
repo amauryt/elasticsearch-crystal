037c8f80151c72abaee3cd827965e151978794b9
Amaury Trujillo <atlemss@gmail.com>
2019-03-26 22:32:12 +0100

    Add default content-type headers to requests with body

--- src/elasticsearch/api/namespace/common.cr
+++ src/elasticsearch/api/namespace/common.cr
@@ -8,7 +8,7 @@ module Elasticsearch
 
       module Constants
         DEFAULT_SERIALIZER = JSON
-  
+
         COMMON_PARAMS = [
           :ignore,                        # Client specific parameters
           :index, :type, :id,             # :index/:type/:id
@@ -17,7 +17,7 @@ module Elasticsearch
           :name,                          # Alias, template, settings, warmer, ...
           :field                          # Get field mapping
         ]
-    
+
         COMMON_QUERY_PARAMS = [
           :ignore,                        # Client specific parameters
           :format,                        # Search, Cat, ...
@@ -25,7 +25,7 @@ module Elasticsearch
           :human,                         # Return numeric values in human readable format
           :filter_path                    # Filter the JSON response
         ]
-  
+
         HTTP_GET          = "GET"
         HTTP_HEAD         = "HEAD"
         HTTP_POST         = "POST"
@@ -48,10 +48,10 @@ module Elasticsearch
       end
 
       class Client
-        def initialize(@settings : Hash(Symbol, String | Int32)) 
+        def initialize(@settings : Hash(Symbol, String | Int32))
         end
 
-        def perform_request(method, path, params={} of String => String, body={} of String => String | Nil) 
+        def perform_request(method, path, params={} of String => String, body={} of String => String | Nil, headers = HTTP::Headers{"Content-Type" => "application/json"})
 
           # normalize params to string
           new_params = {} of String => String
@@ -62,7 +62,7 @@ module Elasticsearch
               new_params[k.to_s] = v.to_s
             end
           end
-          
+
           final_params = HTTP::Params.encode(new_params)
 
           if !body.nil?
@@ -70,34 +70,32 @@ module Elasticsearch
           else
             post_data = nil
           end
-           
-          if method == "GET"
-            endpoint = "http://#{@settings[:host]}:#{@settings[:port]}/#{path}?#{final_params}"
-            response = HTTP::Client.get(endpoint, body: post_data, headers: HTTP::Headers{"Content-Type" => "application/json"})
-          elsif method == "POST"
-            endpoint = "http://#{@settings[:host]}:#{@settings[:port]}/#{path}"
-            response = HTTP::Client.post(url: endpoint, body: post_data)
-          elsif method == "PUT"
-            endpoint = "http://#{@settings[:host]}:#{@settings[:port]}/#{path}"
-            response = HTTP::Client.put(url: endpoint, body: post_data)
-          elsif method == "DELETE"
-            endpoint = "http://#{@settings[:host]}:#{@settings[:port]}/#{path}?#{final_params}"
-            response = HTTP::Client.delete(url: endpoint)
-          elsif method == "HEAD"
-            endpoint = "http://#{@settings[:host]}:#{@settings[:port]}/#{path}"
-            response = HTTP::Client.head(url: endpoint)
-          end
+
+          base_url = "http://#{@settings[:host]}:#{@settings[:port]}/#{path}"
+
+          response = case method
+                      when "GET"
+                        HTTP::Client.get("#{base_url}?#{final_params}", body: post_data, headers: headers)
+                      when "POST"
+                        HTTP::Client.post(url: base_url, body: post_data, headers: headers)
+                      when "PUT"
+                        HTTP::Client.put(url: base_url, body: post_data, headers: headers)
+                      when "DELETE"
+                        HTTP::Client.delete(url: "#{base_url}?#{final_params}")
+                      when "HEAD"
+                        HTTP::Client.head(url: base_url)
+                      end
 
           result = response.as(HTTP::Client::Response)
-         
+
           if result.headers["Content-Type"].includes?("application/json") && method != "HEAD"
             final_response = JsonResponse.new result.status_code, JSON.parse(result.body), result.headers
           else
             final_response = Response.new result.status_code, result.body.as(String), result.headers
           end
-          
+
           final_response
-          
+
         end
       end
     end
